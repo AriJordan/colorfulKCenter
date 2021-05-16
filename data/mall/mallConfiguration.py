2c2d3ea04ed0d84b644e3fa988efb32f461e33f5
Georg Anegg <georg.anegg@gmail.com>
2021-05-16 11:35:58 +0200

    Settings for faster testing

--- data/mall/mallConfiguration.py
+++ data/mall/mallConfiguration.py
@@ -2,7 +2,7 @@ from numpy import array
 mallConfiguration = {
     "algoLetters" : "ro",  #ghcbo",
     "nCenters" : 2,
-    "totalPoints" : 200,
-    "p" : array([20,20]),
+    "totalPoints" : 50,
+    "p" : array([10,20]),
     "fixSeed" : False,
 }
