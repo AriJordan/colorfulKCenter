8ef2a0bf538d3fb7b05b144a9286dd092829ca26
Georg Anegg <georg.anegg@gmail.com>
2021-06-09 11:14:08 +0200

    Added BIPV to Mall to check LP solvers

--- data/mall/mallConfiguration.py
+++ data/mall/mallConfiguration.py
@@ -1,6 +1,6 @@
 from numpy import array
 mallConfiguration = {
-    "algoLetters" : "ro",  #ghcbo",
+    "algoLetters" : "rob",  #ghcbo",
     "nCenters" : 2,
     "totalPoints" : 50,
     "p" : array([10,20]),
