c635fbd9757b74322a58c15a6aaff286f6a93a3f
Georg Anegg <georg.anegg@gmail.com>
2021-06-16 15:52:05 +0200

    Added simplexLPsolver as choice

--- algorithms/LPSolver.py
+++ algorithms/LPSolver.py
@@ -1,3 +1,7 @@
 from algorithms.gurobiLPSolver import gurobiLPSolver
+from algorithms.simplexLPSolver import simplexLPSolver
 
-LPSolver = gurobiLPSolver
+select = 0
+selection = {0: gurobiLPSolver, 1: simplexLPSolver}
+
+LPSolver = selection[select]
